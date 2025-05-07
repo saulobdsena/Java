371cfbc555e1aee3c563d33b1338e5c7256a8b80
Saulo Borges <saulobdsena@gmail.com>
2025-05-07 19:00:32 -0300

    Trabalho

---
+++ TrabalhoPOO/Main.java
@@ -0,0 +1,8 @@
+package TrabalhoPOO;
+public class Main {
+    public static void main(String[] args) {
+        Employee empregado = new Employee("Saulo", "Sena", "1234"); 
+            
+        System.out.println(empregado.toString());
+    }
+}
